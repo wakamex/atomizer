3f5c29b09c73b444519ac4b25dad002534f14b0c
Mihai <mcosma@gmail.com>
2025-06-03 08:27:12 -0400

    remove rename files script

--- cmd/maker_quote_responder/rename_files.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Rename the original market_maker.go to preserve it
-mv market_maker.go market_maker_original.go
-# Rename the refactored file to be the new market_maker.go
-mv market_maker_refactored.go market_maker.go
